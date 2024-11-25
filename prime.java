f3481c42aa5b06db51c5ab586df41b3072595996
sushmitha <sushmithachitneni@gmail.com>
2024-11-25 20:35:43 +0530

    sushi

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int num = 20;
+        int num = 300;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
