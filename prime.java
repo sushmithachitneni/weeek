0e421c0da251967b0c2bb2e3e07c47bd9dcd3780
sushmitha <sushmithachitneni@gmail.com>
2024-11-25 20:31:36 +0530

    sush

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int num = 100;
+        int num = 1;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
